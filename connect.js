4814a0d73aff7bda484229e996164f8fb672019d
jaenurihasrodianto <jaenuri.hasro@gmail.com>
2024-04-14 22:44:48 +0700

    tambah lagi

--- connect.js
+++ connect.js
@@ -1,10 +1,11 @@
 const form = document.querySelector("form");
 const nama = document.getElementById("name");
 const email = document.getElementById("email");
+const subject = document.getElementById("subject");
 const mess = document.getElementById("message");
 
 function sendEmail() {
-  const bodymessage = "Nama Lengkap: ${nama.value}<br> Email: ${email.value}<br> Pesan: ${mess.value}";
+  const bodymessage = "Nama Lengkap: ${nama.value}<br> Email: ${email.value}<br> Subject: ${subject.value}<br> Pesan: ${mess.value}";
 
   Email.send({
     Host: "smtp.elasticemail.com",
