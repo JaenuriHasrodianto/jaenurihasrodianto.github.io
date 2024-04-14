dd7ac048b594cd5e883f75fff09a833b0b8e876d
jaenurihasrodianto <jaenuri.hasro@gmail.com>
2024-04-14 22:37:18 +0700

    connectJS

--- connect.js
+++ connect.js
@@ -1,14 +1,19 @@
 const form = document.querySelector("form");
+const nama = document.getElementById("name");
+const email = document.getElementById("email");
+const mess = document.getElementById("message");
 
 function sendEmail() {
+  const bodymessage = "Nama Lengkap: ${nama.value}<br> Email: ${email.value}<br> Pesan: ${mess.value}";
+
   Email.send({
     Host: "smtp.elasticemail.com",
     Username: "jaenuri.hasro@gmail.com",
     Password: "82389FF06A15624D26592F731E6226233E95",
     To: "jaenuri.hasro@gmail.com",
     From: "jaenuri.hasro@gmail.com",
-    Subject: "This is the subject",
-    Body: "And this is the body",
+    Subject: nama.value,
+    Body: bodymessage,
   }).then((message) => alert(message));
 }
 
