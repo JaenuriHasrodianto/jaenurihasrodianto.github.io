4ccef84af2d9d63451020834cf9a38f98d1875ea
jaenurihasrodianto <jaenuri.hasro@gmail.com>
2024-04-15 19:08:45 +0700

    coba lagi

--- connect.js
+++ connect.js
@@ -5,7 +5,7 @@ const subject = document.getElementById("subject");
 const mess = document.getElementById("message");
 
 function sendEmail() {
-  const bodyMessage = "Nama Lengkap: ${nama.value}<br> Email: ${email.value}<br> Subject: ${subject.value}<br> Pesan: ${mess.value}";
+  const bodyMessage = Nama Lengkap: ${nama.value}<br> Email: ${email.value}<br> Subject: ${subject.value}<br> Pesan: ${mess.value};
 
   Email.send({
     Host: "smtp.elasticemail.com",
