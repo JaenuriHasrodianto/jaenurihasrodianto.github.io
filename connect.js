c90f742b422d4704d764360fce5f901701d5dab8
JAENURI HASRODIANTO <158109506+JaenuriHasrodianto@users.noreply.github.com>
2025-12-11 22:23:57 +0700

    Update connect.js
    coba coding barunya meta

--- connect.js
+++ connect.js
@@ -1,85 +1,83 @@
 const form = document.querySelector("form");
-const nama = document.getElementById("name");
-const email = document.getElementById("email");
-const subject = document.getElementById("subject");
-const mess = document.getElementById("message");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("email");
+const subjectInput = document.getElementById("subject");
+const messageInput = document.getElementById("message");
 
-function sendEmail() {
-  const bodyMessage = `Nama Lengkap: ${nama.value}<br> Email: ${email.value}<br> Pesan: ${mess.value}`;
-  Email.send({
-    SecureToken: "4c41dc55-ad23-4609-b0bb-b358c6d9849a",
-    To: "jaenuri.hasro@gmail.com",
-    From: "jaenuri.hasro@gmail.com",
-    Subject: subject.value,
-    Body: bodyMessage
-  }).then(
-    message => {
-      if (message == "OK") {
-        Swal.fire({
-          title: "SUCCESS!",
-          text: "anda berhasil mengirim pesan!",
-          icon: "success",
-        });
-      }
-    }
-  );
-}
-
-function checkInputs() {
-  const items = document.querySelectorAll(".item");
-
-  for (const item of items) {
-    if (item.value == "") {
-      item.classList.add("error");
-      item.parentElement.classList.add("error");
-    }
-
-    if (items[1].value != "") {
-      checkEmail();
-    }
-
-    items[1].addEventListener("keyup", () => {
-      checkEmail();
-    });
-
-    item.addEventListener("keyup", () => {
-      if (item.value != "") {
-        item.classList.remove("error");
-        item.parentElement.classList.remove("error");
-      } else {
-        item.classList.add("error");
-        item.parentElement.classList.add("error");
-      }
-    });
+const checkName = () => {
+  if (nameInput.value === "") {
+    nameInput.classList.add("error");
+    nameInput.parentElement.classList.add("error");
+  } else {
+    nameInput.classList.remove("error");
+    nameInput.parentElement.classList.remove("error");
   }
-}
+};
 
-function checkEmail() {
+const checkEmail = () => {
   const emailRegex = /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,3})(\.[a-z]{2,3})?$/;
   const errorTxtEmail = document.querySelector(".error-text.email");
-
-  if (!email.value.match(emailRegex)) {
-    email.classList.add("error");
-    email.parentElement.classList.add("error");
-
-    if (email.value != "") {
+  if (!emailInput.value.match(emailRegex)) {
+    emailInput.classList.add("error");
+    emailInput.parentElement.classList.add("error");
+    if (emailInput.value !== "") {
       errorTxtEmail.innerText = "Masukan email yang benar!";
     } else {
       errorTxtEmail.innerText = "input gak boleh kosong yaa!";
     }
   } else {
-    email.classList.remove("error");
-    email.parentElement.classList.remove("error");
+    emailInput.classList.remove("error");
+    emailInput.parentElement.classList.remove("error");
   }
-}
+};
+
+const checkSubject = () => {
+  if (subjectInput.value === "") {
+    subjectInput.classList.add("error");
+    subjectInput.parentElement.classList.add("error");
+  } else {
+    subjectInput.classList.remove("error");
+    subjectInput.parentElement.classList.remove("error");
+  }
+};
+
+const checkMessage = () => {
+  if (messageInput.value === "") {
+    messageInput.classList.add("error");
+    messageInput.parentElement.classList.add("error");
+  } else {
+    messageInput.classList.remove("error");
+    messageInput.parentElement.classList.remove("error");
+  }
+};
+
+const sendEmail = () => {
+  const bodyMessage = `Nama Lengkap: ${nameInput.value}<br> Email: ${emailInput.value}<br> Pesan: ${messageInput.value}`;
+  Email.send({
+    SecureToken: "4c41dc55-ad23-4609-b0bb-b358c6d9849a",
+    To: "jaenuri.hasro@gmail.com",
+    From: "jaenuri.hasro@gmail.com",
+    Subject: subjectInput.value,
+    Body: bodyMessage
+  }).then(message => {
+    if (message === "OK") {
+      Swal.fire({
+        title: "SUCCESS!",
+        text: "anda berhasil mengirim pesan!",
+        icon: "success",
+      });
+    }
+  });
+};
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  checkInputs();
-
-  if (!nama.classList.contains("error") && !email.classList.contains("error") && !subject.classList.contains("error") && !mess.classList.contains("error")) {
+  checkName();
+  checkEmail();
+  checkSubject();
+  checkMessage();
+  if (!nameInput.classList.contains("error") && !emailInput.classList.contains("error") && !subjectInput.classList.contains("error") && !messageInput.classList.contains("error")) {
     sendEmail();
-
     form.reset();
     return false;
   }
